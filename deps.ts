ab2818392b9bb2db6488e513fc543cda37599c54
Vikram T <vikram.january@gmail.com>
2025-05-04 12:55:58 +0300

    Module exporting issues fix- dep.ts

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 // import { config as dotEnvConfig } from 'https://deno.land/x/dotenv@v1.0.1/mod.ts';
 import * as dotEnv from 'https://deno.land/x/dotenv@v1.0.1/mod.ts';
 
-const dotEnvConfig = dotEnv.config();
+// const dotEnvConfig = dotEnv.config();
 
-export default dotEnvConfig;
+export default dotEnv.config;
