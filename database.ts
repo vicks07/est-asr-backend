b20c770d31fab97924054c58a7fdfb4c8f230130
Vikram T <vikram.january@gmail.com>
2025-05-04 13:15:33 +0300

    Minor fixes

--- database.ts
+++ database.ts
@@ -1,4 +1,4 @@
-import { Pool } from "https://deno.land/x/postgres/mod.ts";
+import { Pool } from "https://deno.land/x/postgres@v0.19.5/mod.ts";
 import dotEnvConfig from './deps.ts';
   
 dotEnvConfig({ export: true, safe: true });
--- deps.ts
+++ deps.ts
@@ -3,4 +3,4 @@ import * as dotEnv from 'https://deno.land/x/dotenv@v1.0.1/mod.ts';
 
 // const dotEnvConfig = dotEnv.config();
 
-export const dotEnvConfig = dotEnv.config; 
+export const dotEnvConfig = dotEnv.config;
